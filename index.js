a78f2b1d830166bd0a9aacaf686c4ad7b2e047bc
Morgan-Ngetich <ngetichmorgan6@gmail.com>
2023-10-16 10:32:18 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function(){
+  const changeP = document.getElementById('text').textContent = ("This is really cool!")
+
+
+
+
+  
+}) 
